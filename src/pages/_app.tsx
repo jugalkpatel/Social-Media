4eb168b4a72e3e4d9c97b2e7910efe9bd131d653
jugal <ptjimmy99@gmail.com>
2022-04-23 14:26:56 +0530

    added mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ApolloProvider } from '@apollo/client';
+
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
+import { ModalsProvider } from '@mantine/modals';
+
+import theme from '../styles/theme';
 
 import '../lib/i18n';
 import { useApollo } from '../lib/apollo';
@@ -9,11 +21,46 @@ import 'normalize.css/normalize.css';
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
   const apolloClient = useApollo(pageProps);
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <Head>
+        <title>Rices</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ApolloProvider client={apolloClient}>
+        <ColorSchemeProvider
+          colorScheme={colorScheme}
+          toggleColorScheme={toggleColorScheme}
+        >
+          <MantineProvider
+            theme={{
+              colorScheme: colorScheme === 'dark' ? 'dark' : 'light',
+              ...theme,
+            }}
+            defaultProps={{
+              Button: { color: 'orange' },
+              ActionIcon: { color: 'orange' },
+            }}
+            withGlobalStyles
+            withNormalizeCSS
+          >
+            <NotificationsProvider>
+              <ModalsProvider>
+                <Component {...pageProps} />
+              </ModalsProvider>
+            </NotificationsProvider>
+          </MantineProvider>
+        </ColorSchemeProvider>
+      </ApolloProvider>
+    </>
   );
 };
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+import { MantineThemeOverride } from '@mantine/core';
+
+const theme: MantineThemeOverride = {
+  primaryColor: 'orange',
+  breakpoints: {
+    xs: 0,
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
+  },
+};
+
+export default theme;
